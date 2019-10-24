2b9ce7a52074dbe2785d2cf3636ec621b7b4b2cf
Alexandre Esteves <alexandre.fmp.esteves@gmail.com>
2019-10-24 19:18:44 +0100

    Reproduce odd cabal/nix bug for conditional dependency

--- default.nix
+++ default.nix
@@ -23,4 +23,18 @@ reflex-platform.project ({ pkgs, ... }: {
     ghc = ["common" "backend" "frontend"];
     ghcjs = ["common" "frontend"];
   };
+
+  overrides = self: super: {
+    dec = self.callHackageDirect {
+      pkg = "dec";
+      ver = "0.0.3";
+      sha256 = "11b8g4nm421pr09yfb4zp18yb7sq4wah598fi3p5fb64yy4c2n4s";
+    } {};
+
+    singleton-bool = self.callHackageDirect {
+      pkg = "singleton-bool";
+      ver = "0.1.5";
+      sha256 = "1kjn5wgwgxdw2xk32d645v3ss2a70v3bzrihjdr2wbj2l4ydcah1";
+    } {};
+  };
 })
